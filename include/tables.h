a6595a6e9f56b15ef83f9b0cbe73e50699393447
Jiahonzheng <jiahonzheng@gmail.com>
2019-10-13 19:50:17 +0800

    feat: 增强 Tables 可读性
    添加 Tables 部分的注释，增强可读性。

--- include/tables.h
+++ include/tables.h
@@ -1,22 +1,31 @@
 #ifndef TABLES_H_
 #define TABLES_H_
 
+// 初始置换
 extern const char IP[];
 
-extern const char PI[];
-
-extern const char P[];
-
-extern const char E[];
-
-extern const char S[8][64];
-
-extern const char PERM_REMOVE_PARITY[];
-
+// PC-1 置换
 extern const char PC1[];
 
+// PC-2 置换
 extern const char PC2[];
 
+// 去除奇偶位
+extern const char PERM_REMOVE_PARITY[];
+
+// 移动位数
 extern const char iteration_shift[];
+
+// E-扩展置换
+extern const char E[];
+
+// S-盒置换
+extern const char S[8][64];
+
+// P-盒置换
+extern const char P[];
+
+// 逆置换
+extern const char PI[];
 
 #endif  // TABLES_H_
